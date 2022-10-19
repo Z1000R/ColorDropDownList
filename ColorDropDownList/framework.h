3fedf2197994e55be148436ac98d97033b02b7c6
Z1000R <tgreenkz1000r@gmail.com>
2022-10-19 17:16:43 +0900

    カラーコード表示、取得処理を変更。開発環境をVS2022,Win11に変更

---
+++ ColorDropDownList/framework.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "targetver.h"
+
+#include <windows.h>
+#include <commctrl.h>
+
+#pragma comment(lib, "comctl32.lib")
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <format>
